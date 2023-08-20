011b4071f8ad90ed5a3f9b61261099b2c1a10607
Geert Bevin <gbevin@uwyn.com>
2023-08-19 23:51:23 -0400

    Updated for bld 1.7.2

--- src/bld/java/rife/bld/extension/TestsBadgeBuild.java
+++ src/bld/java/rife/bld/extension/TestsBadgeBuild.java
@@ -16,7 +16,7 @@ public class TestsBadgeBuild extends Project {
     public TestsBadgeBuild() {
         pkg = "rife.bld.extension";
         name = "TestsBadge";
-        version = version(1,4,1);
+        version = version(1,4,2);
         archiveBaseName = "bld-tests-badge";
 
         javaRelease = 17;
@@ -25,7 +25,7 @@ public class TestsBadgeBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(compile)
-            .include(dependency("com.uwyn.rife2", "bld", version(1,7,1)));
+            .include(dependency("com.uwyn.rife2", "bld", version(1,7,2)));
         scope(test)
             .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,9,3)))
             .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,9,3)));
